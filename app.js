c7a93ca8a71b0650c0c82affa8094630b00aeb6f
CRHarding <casey.r.harding@gmail.com>
2018-04-29 13:12:17 -0400

    Initial express scaffolding

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const PORT = process.env.PORT || 3001;
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get('/*', (req, res) => {
+  res.render('MADE IT!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
+
+module.exports = app;
