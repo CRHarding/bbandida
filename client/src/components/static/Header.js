0562eb3ae4b6875c18c0982ec2c6bdfa079c744d
TomChoe <doodeitstom@yahoo.com>
2018-04-29 21:52:29 -0400

    mocked a quick nav bar

--- client/src/components/static/Header.js
+++ client/src/components/static/Header.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../images/bbandida.jpeg';
 
 const Header = () => {
 	return (
 		<div class="header">
-			<nav>
-				<Link to = "/about">
-					About
-				</Link>
-				<Link to = "/contact">
-					Contact
-				</Link>
-				<Link to = "/blogs">
-					Blogs
-				</Link>
-				<Link to = "/cart">
-					Cart
-				</Link>
+			<nav className="white">
+				<div className="nav-wrapper">
+					<a href="/" class="brand-logo center"><img src={ logo } height="96" width="80" /></a>
+					<ul id="nav-mobile" className="left hide-on-med-and-down">
+						<li><Link to = "/about" className="blue-text">
+							About
+						</Link></li>
+						<li><Link to = "/contact" className="blue-text">
+							Contact
+						</Link></li>
+					</ul>
+					<ul id="nav-mobile" className="right hide-on-med-and-down">
+						<li><Link to = "/blogs" className="blue-text">
+							Blogs
+						</Link></li>
+						<li><Link to = "/cart" className="blue-text">
+							Cart
+						</Link></li>
+					</ul>
+				</div>
 			</nav>
-			<hr />
 		</div>
 	)
 }
