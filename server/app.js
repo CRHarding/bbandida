d69e2d1fe39df6f6c9e6e27fa23130752a268427
CRHarding <casey.r.harding@gmail.com>
2018-06-26 13:16:48 -0400

    Deploy

--- server/app.js
+++ server/app.js
@@ -17,13 +17,17 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, 'client/build'));
 
 
 app.use('/api/products', productsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/contributors', contributorRouter);
 app.use('/api/send', contactRouter);
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.get('/*', (req, res) => {
   res.json('MADE IT!');
